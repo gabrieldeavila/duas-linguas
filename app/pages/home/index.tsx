1ca454d880ad18f328362a4f55e63e00caf73827
geavila <gabrielavila04@protonmail.com>
2025-10-31 22:42:32 -0300

    feat: implement user session check and redirect to dashboard on home page

--- app/pages/home/index.tsx
+++ app/pages/home/index.tsx
@@ -1,15 +1,28 @@
+import { useEffect } from "react";
+import { supabase } from "~/lib/supabase";
 import Cta from "./components/cta";
 import Features from "./components/features";
 import Footer from "./components/footer";
 import Header from "./components/header";
 import HeroHome from "./components/hero";
 import Workflows from "./components/workflow";
+import { useNavigate } from "react-router";
 
 export function meta() {
   return [{ title: "Duas Linguas" }];
 }
 
 function Home() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    supabase.auth.getSession().then((session) => {
+      if (session?.data?.session?.user) {
+        navigate("/dashboard");
+      }
+    });
+  }, [navigate]);
+
   return (
     <>
       <Header />
